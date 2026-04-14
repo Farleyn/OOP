59e8dbb4b9cbbee050a372e183e626b065dbc668
opilane <opilane@HP-CZC2349HT4>
2026-04-14 09:53:18 +0300

    Add project files.

---
+++ ConsoleApp1/Isik.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Isik
+    {
+        // Privaatne väli - otse ligi ei saa
+        private int sünniaasta;
+
+        public string Nimi { get; set; }
+
+        public void NimiKüsi ()
+        {
+            Console.WriteLine("Sisesta sinu nimi:");
+            Nimi = Console.ReadLine();
+        }
+
+        public void Tervita()
+        {
+            Console.WriteLine($"Tere! Mina olen {Nimi} ja ma olen {Vanus} aastat vana.");
+        }
+
+        // Kontrollitud omadus
+        public int Sünniaasta
+        {
+            get { return sünniaasta; }
+            set
+            {
+                if (value > 1900 && value <= DateTime.Now.Year)
+                    sünniaasta = value;
+                else
+                    Console.WriteLine("Vigane sünniaasta!");
+            }
+        }
+
+        // Arvutatud omadus (ainult lugemiseks / getter)
+        public int Vanus => DateTime.Now.Year - sünniaasta;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Isik inimene1 = new Isik();
+            inimene1.NimiKüsi();
+            inimene1.Sünniaasta = 2009;
+            inimene1.Tervita();
+            Õpetaja õpetaja1 = new Õpetaja();
+            õpetaja1.Nimi = "Filipp";
+            õpetaja1.Sünniaasta = 1980;
+            õpetaja1.Aine = "Matemaatika";
+            õpetaja1.Õpeta();
+            õpetaja1.Tervita();
+            Õpilane õpilane1 = new Õpilane();
+            õpilane1.Kool = "Avatud Kool";
+            õpilane1.Klass = 9;
+        }
+    }
+}
